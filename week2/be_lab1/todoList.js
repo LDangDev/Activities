d5305b5cfbab1455e01f28a9cb184a0fcb0b9d72
LDangDev <55139095+LDangDev@users.noreply.github.com>
2024-09-01 16:20:17 +0300

    Add files via upload

---
+++ week2/be_lab1/todoList.js
@@ -0,0 +1,80 @@
+// {
+//     "task": "Buy groceries",
+//     "completed": false,
+//     "dueDate": "2024-08-30"
+// }
+
+let todosArray = []
+let todoId = 1
+
+
+function getAll() {
+    return todosArray;
+}
+
+function addOne(task, completed, dueDate) {
+    if(!task || !dueDate || (completed !== false || completed !== true)){
+        return false
+    }
+    const newTodo = {
+        id: todoId++,
+        task,
+        completed,
+        dueDate
+    }
+
+    todosArray.push(newTodo)
+    return newTodo
+}
+
+function findById(id) {
+    return result = todosArray.find(todo => todo.id === id) || false
+}
+
+function updateOneById(id, updateData) {
+    const todo = findById(id)
+
+    if(updateData.task) {
+        todo.task = updateData.task
+    }
+    if(updateData.completed) {
+        todo,completed = updateData.completed
+    }
+    if(updateData.dueDate) {
+        todo,dueDate = updateData.dueDate
+    }
+    return todo
+}
+
+function deleteOneById(id) {
+    const todo = findById(id)
+    if(todo) {
+        const initialLength = todosArray.length
+        todosArray = todosArray.fillter(todo => todo.id !== Number(id))
+        return todosArray.length < initialLength
+    }
+    return False
+}
+
+
+if(require.main === module) {
+    // console.log("getAll called:", getAll())
+    addOne('dfsdf', 0, "2024-08-30")
+    addOne("exercise", false, "2024-08-30")
+    addOne("sleep", true, "2024-08-30")
+    addOne("test", false, "2024-08-30")
+    addOne("undefined", undefined, "2024-08-30")
+    console.log("List of todos: ",todosArray)
+    console.log("Find todo id 3: ", findById(3))
+    console.log("Find todo id 10: ", findById(10))
+}
+
+const ToDos = {
+    getAll,
+    addOne,
+    findById,
+    updateOneById,
+    deleteOneById
+};
+
+module.exports = ToDos;
