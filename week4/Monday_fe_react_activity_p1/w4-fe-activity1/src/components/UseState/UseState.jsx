c82e95feb5c8252b399c1464205fc6e6feb2ef94
locphuocdang <dangloc2407@gmail.com>
2024-09-09 10:42:52 +0300

    fe-react-activity1

---
+++ week4/Monday_fe_react_activity_p1/w4-fe-activity1/src/components/UseState/UseState.jsx
@@ -0,0 +1,47 @@
+import './UseState.css';
+import { useState } from 'react';
+
+const UseState = () => {
+  // console.log(useState('light'))
+  
+  const [theme, setTheme] = useState('light')
+  const [num, setNum] = useState(0)
+
+  const handleSetDarkTheme = () => {
+    setTheme('dark')
+  }
+
+  const handleSetLightTheme = () => {
+    setTheme('light')
+  }
+
+  const handleIncrement = () => {
+    setNum(prevNum => prevNum + 1)
+  }
+
+  const handleDecrement = () => {
+    setNum(prevNum => prevNum - 1)
+  }
+
+  const handleToggleTheme = () => {
+    setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <div className={`state ${theme}`}>
+      <h1>UseState Component</h1>
+      <button onClick={handleSetDarkTheme}>Dark</button>
+      <button onClick={handleSetLightTheme}>Light</button>
+      <button onClick={handleToggleTheme}>Toggle Theme</button>
+      <h2>DISPLAY COUNT HERE: {num}</h2>
+      <button onClick={handleIncrement}>
+        Increment
+      </button>
+      <button onClick={handleDecrement}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default UseState;
