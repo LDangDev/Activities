50bf958ca28d7a59aaead4fa0003eecb0ed50d0c
locphuocdang <dangloc2407@gmail.com>
2024-09-09 10:44:54 +0300

    change folder name

--- week4/Monday_morning_activities/w4-fe-activity1/src/components/UseState/UseState.jsx
+++ week4/Monday_morning_activities/w4-fe-activity1/src/components/UseState/UseState.jsx
@@ -1,31 +1,31 @@
-import './UseState.css';
-import { useState } from 'react';
+import "./UseState.css";
+import { useState } from "react";
 
 const UseState = () => {
   // console.log(useState('light'))
-  
-  const [theme, setTheme] = useState('light')
-  const [num, setNum] = useState(0)
+
+  const [theme, setTheme] = useState("light");
+  const [num, setNum] = useState(0);
 
   const handleSetDarkTheme = () => {
-    setTheme('dark')
-  }
+    setTheme("dark");
+  };
 
   const handleSetLightTheme = () => {
-    setTheme('light')
-  }
+    setTheme("light");
+  };
 
   const handleIncrement = () => {
-    setNum(prevNum => prevNum + 1)
-  }
+    setNum((prevNum) => prevNum + 1);
+  };
 
   const handleDecrement = () => {
-    setNum(prevNum => prevNum - 1)
-  }
+    setNum((prevNum) => prevNum - 1);
+  };
 
   const handleToggleTheme = () => {
-    setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light')
-  }
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   return (
     <div className={`state ${theme}`}>
@@ -34,12 +34,8 @@ const UseState = () => {
       <button onClick={handleSetLightTheme}>Light</button>
       <button onClick={handleToggleTheme}>Toggle Theme</button>
       <h2>DISPLAY COUNT HERE: {num}</h2>
-      <button onClick={handleIncrement}>
-        Increment
-      </button>
-      <button onClick={handleDecrement}>
-        Decrement
-      </button>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 };
